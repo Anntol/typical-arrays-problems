6c36af98b531405314604e2383ba188977e0af34
Anna Tolstova <anna_tolstova@hotmail.com>
2020-09-29 17:44:19 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length) {
+    let result = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < result) result = array[i];
+    }
+    return result;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length) {
+    let result = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > result) result = array[i];
+    }
+    return result;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length) {
+    let sum = array.reduce((sum, current) => sum + current, 0);
+    return sum / array.length;
+  }
   return 0;
 }
